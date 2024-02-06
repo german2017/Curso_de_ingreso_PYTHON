82a47d9b4cc2c3730cf779f3412937e9ebe95927
german2017 <scarafiloutn@gmail.com>
2024-02-06 11:26:07 -0300

    lalal

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_02.py
@@ -27,8 +27,10 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        alert("jdjjd", "jsjsj")
-        pass
+        #snake_case
+        nombre_alumno = prompt("Datos", "Ingrese su nombre")
+        alert("Datos", nombre_alumno)
+        
         
         
     
